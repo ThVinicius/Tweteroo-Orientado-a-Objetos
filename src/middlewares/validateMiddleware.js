d252fff96b2c7257800be7d61baf9579762239c4
ThVinicius <vinicioss9955@gmail.com>
2022-11-26 16:15:24 -0300

    refactor: create middleware

---
+++ src/middlewares/validateMiddleware.js
@@ -0,0 +1,23 @@
+export default class validateMiddleware {
+  constructor() {}
+
+  signUp(req, res, next) {
+    const { tweet, username } = req.body
+
+    if (!username || !tweet) {
+      return res.status(400).send('Todos os campos são obrigatórios!')
+    }
+
+    next()
+  }
+
+  getTweets(req, res, next) {
+    const { page } = req.query
+
+    if (page && page < 1) {
+      return res.status(400).send('Informe uma página válida!')
+    }
+
+    next()
+  }
+}
